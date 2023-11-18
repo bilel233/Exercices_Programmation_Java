f6aee308d2ae8181d736ffe5eef66b1e0772a538
bilel233 <bilel.mjdb@gmail.com>
2023-11-18 18:42:04 +0100

    Fin de l'exercice 1

--- src/Exercice1.java
+++ src/Exercice1.java
@@ -1,3 +1,16 @@
-public class Exercice1 {
+/* Ecrire un programme java qui demande à l'utilisateur de saisir son nom et
+de lui afficher son nom avec le message de bienvenue
+ */
 
+import java.util.Scanner;
+
+public class Exercice1 {
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Veuillez saisir votre nom");
+        String nom = scanner.nextLine();
+        System.out.println("votre nom est "+nom);
+        scanner.close();
+    }
 }
