bc75ac99b7fc449eb15c879093e78957b4bf9090
bilel233 <bilel.mjdb@gmail.com>
2023-11-20 09:22:36 +0100

    fin de l'exercice 9

---
+++ src/Exercice9.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Exercice9 {
+
+    public static void main(String[] args) {
+        int n = 0;
+        // 1 ere instruction
+
+        do {
+            n++;
+            System.out.println(n);
+
+        }
+        while (n < 10);
+    }
+
+}
