77e13fa50d5b6b3346cb0f190b0b337321ff2ef2
bilel233 <bilel.mjdb@gmail.com>
2023-11-28 09:57:15 +0100

    Creation de la classe Bonjour

---
+++ src/Bonjour.java
@@ -0,0 +1,9 @@
+public class Bonjour {
+    public static void main(String[] args)
+    {
+        System.out.println("Bonjour Monde");
+
+    }
+}
+// la methode main sert a executer le code principal du programme
+
