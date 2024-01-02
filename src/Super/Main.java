3192293d531cb77f3686dd81d930b271fa806ad9
bilel233 <bilel.mjdb@gmail.com>
2024-01-02 20:42:58 +0100

    Super keyword

---
+++ src/Super/Main.java
@@ -0,0 +1,11 @@
+package Super;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Hero hero1 = new Hero("Batman",42,"€€€");
+
+        System.out.println(hero1.name);
+        System.out.println(hero1.toString());
+    }
+}
