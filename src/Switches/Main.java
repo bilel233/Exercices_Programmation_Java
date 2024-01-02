cc7571c6c1f26ca2156f740bccc2e6b440e61c99
bilel233 <bilel.mjdb@gmail.com>
2024-01-02 12:28:36 +0100

    Switches

---
+++ src/Switches/Main.java
@@ -0,0 +1,23 @@
+package Switches;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        String day = "pizza";
+        switch(day)
+        {
+            case "Sunday" : System.out.println("It is Sunday");
+            break;
+
+            case "Monday" :  System.out.println("It is Monday");
+            break;
+
+            case "Tuesday" :  System.out.println("It is Tuesday");
+            break;
+
+            case "Friday" : System.out.println("It is Friday");
+            break;
+            default:System.out.println("That is not a day");
+        }
+    }
+}
