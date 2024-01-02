1fff231e2905c5c9b9e75936cf77da3d09de6f0f
bilel233 <bilel.mjdb@gmail.com>
2024-01-02 12:20:01 +0100

    if statements

---
+++ src/ifStatements/Main.java
@@ -0,0 +1,22 @@
+package ifStatements;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        int age = 18;
+
+        if(age >=75){
+            System.out.println("Ok boomer");
+        }
+        else if(age >=18){
+            System.out.println("you're an adult");
+        }
+        else if(age >=13)
+        {
+            System.out.println("you are a teenager !");
+        }
+        else{
+            System.out.println("you're not an adult");
+        }
+    }
+}
