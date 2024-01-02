5c786ce6f0a1c0fc898e4f2410a4e2e06d3a2ef5
bilel233 <bilel.mjdb@gmail.com>
2024-01-02 20:52:48 +0100

    abstract classes

---
+++ src/Abstraction/Main.java
@@ -0,0 +1,11 @@
+package Abstraction;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Car car = new Car();
+       // Vehicle vehicle = new Vehicle();
+
+        car.go();
+    }
+}
