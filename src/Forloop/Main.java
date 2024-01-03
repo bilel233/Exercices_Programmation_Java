68f02b4f2774a61843a8809ff0b2787ae43016a5
bilel233 <bilel.mjdb@gmail.com>
2024-01-03 22:09:14 +0100

    for loops

---
+++ src/Forloop/Main.java
@@ -0,0 +1,12 @@
+package Forloop;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        for(int i=10;i>=0;i-=2)
+        {
+            System.out.println(i);
+        }
+        System.out.println("Happy New Year !!!!!");
+    }
+}
